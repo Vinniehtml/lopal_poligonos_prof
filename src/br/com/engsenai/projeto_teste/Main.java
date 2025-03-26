b416e7a611ba2313d1469c8f77fd4bdcbd350f86
VINICIUS AGUIAR DE CRUZ <25133960@cfp127aluno.local>
2025-03-26 17:00:49 -0300

    Implementação do scanner

--- src/br/com/engsenai/projeto_teste/Main.java
+++ src/br/com/engsenai/projeto_teste/Main.java
@@ -1,11 +1,23 @@
 package br.com.engsenai.projeto_teste;
 
+import java.util.Scanner;
+
+import br.com.engsenai.projeto_teste.ui.Menu;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite o seu nome"); 
+		String nome = scanner.next();
+
+		System.out.println(nome.length());
+		System.out.println("Olá, " + nome);
+		System.out.println(nome.toUpperCase());
+		
+		Menu menu = new Menu();
+		menu.criarMenu();
 	}
 
 }
